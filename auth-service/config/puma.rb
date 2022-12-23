cef43bb9d384acd3d99b4a3a20bd7dc941266bf4
Georgi Ganchev <googs.bs@gmail.com>
2022-12-23 12:23:14 +0000

    chore(auth-service): changes puma port

--- auth-service/config/puma.rb
+++ auth-service/config/puma.rb
@@ -11,7 +11,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port ENV.fetch("PORT") { 3004 }
+port ENV.fetch("PORT") { 5002 }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
