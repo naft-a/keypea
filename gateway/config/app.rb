e1eb4cb9a3095b7a720a2a4ea607440d67953cbd
Georgi Ganchev <googs.bs@gmail.com>
2022-12-23 12:30:38 +0000

    chore(gateway): changes puma port; adds cors

--- gateway/config/app.rb
+++ gateway/config/app.rb
@@ -3,9 +3,21 @@
 require "hanami"
 require "pry-remote"
 require "active_support/time"
+require "rack/cors"
 
 module Gateway
   class App < Hanami::App
+
     config.middleware.use :body_parser, :json
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins "https://frontend.localhost"
+        resource "*",
+                 headers: "any",
+                 methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
   end
 end
