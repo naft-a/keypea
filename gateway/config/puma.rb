d70f0602077682653121435e267a1b3459574836
Georgi Ganchev <googs.bs@gmail.com>
2023-03-28 19:18:18 +0100

    fix(gateway): puma port

--- gateway/config/puma.rb
+++ gateway/config/puma.rb
@@ -4,7 +4,7 @@ max_threads_count = ENV.fetch("PUMA_MAX_THREADS", 5)
 min_threads_count = ENV.fetch("PUMA_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-port        ENV.fetch("PUMA_PORT", 5001)
+port        ENV.fetch("PUMA_PORT", ENV.fetch("PORT", 5001))
 environment ENV.fetch("HANAMI_ENV", "development")
 workers     ENV.fetch("PUMA_WORKERS", 2)
 
